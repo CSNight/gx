a2be1428bc62138c099b9f532b6b3271fc6dc677
chensi <chensi@thtf.com.cn>
2020-08-11 17:30:50 +0800

    try register node dynamic

--- src/components/shape/flowNode.js
+++ src/components/shape/flowNode.js
@@ -24,25 +24,30 @@ const taskDefaultOptions = {
     }
 };
 
-export default function (G6) {
-    G6.registerNode('task-node', {
-        shapeType: 'rect',
-        options: {
-            ...taskDefaultOptions
-        },
-        getShapeStyle(cfg) {
-            cfg.size = [80, 44];
-            const width = cfg.size[0];
-            const height = cfg.size[1];
-            const style = {
-                x: 0 - width / 2,
-                y: 0 - height / 2,
-                width,
-                height,
-                ...this.options.style,
-            };
-            return style;
-        },
 
-    }, 'base-node');
+export default function (G6) {
+    for (let i = 0; i < 1; i++) {
+        G6.registerNode('tk-node', {
+            shapeType: 'rect',
+            options: {...taskDefaultOptions},
+            getShapeStyle: function (cfg) {
+                if (cfg.size && typeof cfg.size === 'string') {
+                    cfg.size = [parseInt(cfg.size.split('*')[0]), parseInt(cfg.size.split('*')[1])]
+                } else if (!cfg.size) {
+                    cfg.size = [80, 44];
+                }
+                const width = cfg.size[0];
+                const height = cfg.size[1];
+                const style = {
+                    x: 0 - width / 2,
+                    y: 0 - height / 2,
+                    width,
+                    height,
+                    ...this.options.style,
+                };
+                return style;
+            },
+        }, 'base-node')
+    }
+
 }
