710e4110d998cff1b6a8624f0e4aa567c6360fc2
csnight <923739637@qq.com>
2020-09-13 21:53:26 +0800

    change register func params

--- src/editor/lib/shape/index.js
+++ src/editor/lib/shape/index.js
@@ -2,8 +2,11 @@ import registerAnchor from './anchor'
 import registerNode from './node'
 import registerEdge from './edge'
 
-export default function (G6) {
-    registerAnchor();
-    registerNode(G6);
-    registerEdge(G6);
+registerAnchor();
+export default function (G6, _shapeDef) {
+    if (_shapeDef.shape_type === "node") {
+        registerNode(G6, _shapeDef);
+    } else {
+        registerEdge(G6, _shapeDef);
+    }
 }
