007a6dd52a2caff9f7b85bb98181bffb9cc81d92
csnight <923739637@qq.com>
2020-09-16 19:21:18 +0800

    change tooltip from behavior to plugins

--- src/editor/lib/behavior/tooltip.js
+++ src/editor/lib/behavior/tooltip.js
@@ -1,16 +1,13 @@
-export const NodeTooltip = {
-    type: 'tooltip', formatText: (e) => {
-        return '<div class="g6-component-tooltip">' +
-            '<h4 class="tooltip-type">ID:' + e.id + '</h4>' +
-            '<div class="tooltip-id">label:' + e.label + '</div>' +
-            '<div class="tooltip-id">type:' + e.type + '</div></div>'
-    }
+export function formatNodeTooltip(e) {
+    return '<div>' +
+        '<h4 class="tooltip-type">ID:' + e.id + '</h4>' +
+        '<div class="tooltip-id">label:' + e.label + '</div>' +
+        '<div class="tooltip-id">type:' + e.type + '</div></div>'
 }
-export const EdgeTooltip = {
-    type: 'edge-tooltip', formatText: (e) => {
-        return '<div class="g6-component-tooltip">' +
-            '<h4 class="tooltip-type">ID:' + e.id + '</h4>' +
-            '<div class="tooltip-id">label:' + e.label + '</div>' +
-            '<div class="tooltip-id">type:' + e.type + '</div></div>'
-    }
+
+export function formatEdgeTooltip(e) {
+    return '<div>' +
+        '<h4 class="tooltip-type">ID:' + e.id + '</h4>' +
+        '<div class="tooltip-id">label:' + e.label + '</div>' +
+        '<div class="tooltip-id">type:' + e.type + '</div></div>'
 }
